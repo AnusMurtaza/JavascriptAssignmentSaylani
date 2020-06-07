eb1fde90cb08073516ad7ec322f98aa83702b4b8
AnusMurtaza <anas.aq97@gmail.com>
2020-06-07 17:21:07 +0500

    Assignment Javascript

---
+++ Assignment#01/app.js
@@ -0,0 +1,9 @@
+alert( "Hello ! Welcome")
+
+alert( "Error! Please enter a valid password")
+
+alert( "Welcome to JS land...\nHappy Coding!" )
+
+alert( "Welcome to JS land")
+
+alert( "Happy Coding!\n")
