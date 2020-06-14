3ef76fa3da74902bb8debe46507e882b4d25399a
AnusMurtaza <anas.aq97@gmail.com>
2020-06-14 16:59:31 +0500

    Now complete another tasks

--- Assignment#01/app.js
+++
@@ -1,9 +0,0 @@
-alert( "Hello ! Welcome")
-
-alert( "Error! Please enter a valid password")
-
-alert( "Welcome to JS land...\nHappy Coding!" )
-
-alert( "Welcome to JS land")
-
-alert( "Happy Coding!\n")
---
+++ app.js
@@ -0,0 +1,601 @@
+
+
+function assignment01task1() {
+    alert("Hello ! Thanks To Visit")
+};
+
+function assignment01task2() {
+    alert("Error! Please enter a valid password")
+};
+
+function assignment01task3() {
+    alert("Welcome to JS land...\nHappy Coding!")
+
+};
+
+function assignment01task4() {
+    alert("Welcome to JS land")
+    alert("Happy Coding! ")
+};
+
+function assignment01task4() {
+    alert("Welcome to JS land")
+    alert("Happy Coding!\n")
+};
+
+// ****************************************************1st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+function assignment02task1() {
+    document.write("var username;")
+};
+
+function assignment02task2() {
+    document.write('var myName = "Anus Murtaza";')
+};
+
+function assignment02task3() {
+    document.write('var message = "Hello World";')
+
+};
+
+
+function assignment02task3c() {
+    var message = "Hello World";
+    alert(message)
+
+};
+
+function assignment02task4() {
+
+    var name = "Anus";
+    var age = 20;
+    var course = "Certified Mobile Application Development";
+
+    alert(name)
+    alert(age)
+    alert(course)
+
+};
+
+function assignment02task5() {
+    var adds = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+    alert(adds)
+
+};
+
+
+function assignment02task6() {
+    var email = "anas.aq97@gmail.com";
+
+    alert("My email address is " + email)
+
+};
+
+
+
+
+function assignment02task7() {
+    var book = "A smarter way to learn JavaScript";
+
+    alert("I am trying to learn from the book " + book)
+
+};
+
+
+
+
+function assignment02task8() {
+
+    document.write("Yah! I can write HTML content through Javascript");
+};
+
+
+
+
+function assignment02task9() {
+    alert("▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+};
+
+
+
+
+
+
+// ****************************************************2nd ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+function assignment03task1() {
+    var age = 20;
+    alert(age)
+};
+
+function assignment03task2() {
+    var visit;
+    visit = 14;
+    alert("You have visited this site " + visit + " times")
+};
+
+function assignment03task3() {
+    var birthYear = 1997;
+    document.write("My birth year is " + birthYear + "<br>")
+    document.write("Data type of my declared variable is " + typeof birthYear)
+};
+
+function assignment03task4() {
+    var name = "John Doe";
+    var product = "T-shirt";
+    var quantity = 5;
+
+
+    document.write( name  + " ordered " + quantity + " " + product + "(s) on XYZ Clothing store")
+};
+
+
+
+
+// ****************************************************3nd ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+function assignment04task1() {
+    var w, x, y;
+    document.write('var w,x,y;');
+};
+
+function assignment04task2() 
+{ 
+   document.write ("<h1>5 legal & 5 illegal <h1> <br> <h2>legal variable names</h2>name <br>taskFour <br>$student <br>roll_no <br>king12<h2>illegal variable names</h2>12345<br>45hello<br>pakistan zindabad<br>%percentage<br>plus+")
+};
+
+function assignment04task3() {
+   document.write("<h1>Rules for naming JS variables</h1>")
+   document.write("Variable names can only contain, number, $ and _. For example : $my_1stVariable <br>")
+   document.write("Variables must begin with a letter,$ or _. For example : $name, _name or name <br>")
+   document.write(" Variable names are case sensitive <br>")
+   document.write("Variable names should not be JS keywords ")
+   
+
+};
+
+// ****************************************************4st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+function assignment05task1() {
+    var a = 3;
+    var b = 5;
+    var sum = a + b;
+
+    document.write("Sum of " + a + " and " + b + " is " + sum);
+
+};
+
+function assignment05task2() {
+    var a = 3;
+    var b = 5;
+    var sub = a - b;
+    var mul = a * b;
+    var div = a / b;
+    var mod = a % b;
+
+
+    document.write("Subtraction of (" + a + " - " + b + ") = " + sub + "<br>");
+    document.write("Multiplication of (" + a + " x " + b + ") = " + mul + "<br>");
+    document.write("Division of (" + a + " / " + b + ") = " + div + "<br>");
+    document.write("Modulus of (" + a + " % " + b + ") = " + mod);
+
+
+};
+
+function assignment05task3() {
+  
+
+    d = 2;  
+
+    n = 1;
+    
+     
+        while(n<=10, d<=5)
+    {
+    
+    document.write(d+ "x" + n + "=" + d*n + "<BR>")
+    
+    n++;
+    
+     
+    
+    if (n == 10)
+    
+    {
+        d++;
+        n = 0;
+    
+    document.write(d+ "x" + n + "=" + d*n + "<BR>")
+    
+    }
+    
+    }
+    
+
+};
+
+function assignment05task4() 
+{
+    var ticketPrice = 600;
+    var ticket = prompt("How many tickets do you want to buy?");
+    var cost =ticket*ticketPrice ;
+
+    document.write("Total cost to buy "+ ticket +" tickets to a movie is " + cost + "PKR")
+};
+
+function assignment05task5() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+function assignment06task1() {
+    var a = 10;
+    var b = ++a;
+    var c = a++;
+    var d = --a;
+    var e = a--;
+
+
+    document.write("Result: <br>");
+    document.write("The value of a is " + a + "<br><br>");
+    document.write("******************************** <br> <br>");
+
+    document.write("The value of ++a is : " + b + "<br>");
+    document.write("Now the value of a is : " + b + "<br>");
+
+    document.write("The value of a++ is : " + c + "<br>");
+    document.write("Now the value of a is : " + c + "<br>");
+
+    document.write("The value of --a is : " + d + "<br>");
+    document.write("Now the value of a is : " + d + "<br>");
+
+    document.write("The value of a-- is : " + e + "<br>");
+    document.write("Now the value of a is : " + e + "<br>");
+
+
+};
+
+function assignment6_9task2() {
+
+};
+
+function assignment6_9task3() {
+    var userName = prompt("Enter Your Name")
+    alert("THANKS TO VISIT " + userName)
+
+};
+
+function assignment6_9task4() {
+    var subject = ["English", "Maths", "Urdu"];
+    var marks = ["100"];
+};
+
+function assignment6_9task5() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+function assignment9_11task1() {
+    var cityName = prompt("Enter City Name");
+
+    if (cityName === "karachi") {
+        alert("Welcome to city of lights")
+    }
+    else {
+        alert("Welcome to " + cityName)
+    }
+};
+
+function assignment9_11task2() {
+    var gender = prompt("Enter Your Gender");
+
+    if (gender === "male") {
+        alert("Good Morning Sir")
+    }
+
+    else if (gender === "female") {
+        alert("Good Morning Ma'am")
+    }
+    else {
+        alert("Please Enter gender like male/female")
+    }
+};
+
+function assignment9_11task3() {
+
+
+};
+
+function assignment9_11task4() {
+    var fuel = prompt("Please Enter Remaining Fuel In Your Car (in litres)");
+    if (fuel < 0.25) {
+        alert("Please refill the fuel in your car")
+    }
+    else {
+        alert("when fuel comes out of 0.25 litres, then refuel your car")
+    }
+
+};
+
+function assignment9_11task5() {
+
+    var a = 4; if (++a === 5) { alert("given condition for variable a is true"); }
+
+
+    var b = 82; if (b++ === 83) { alert("given condition for variable b is true"); }
+    var c = 12; if (c++ === 13) { alert("condition 1 is true"); }
+    if (c === 13) { alert("condition 2 is true"); }
+    if (++c < 14) { alert("condition 3 is true"); }
+    if (c === 14) { alert("condition 4 is true"); }
+    var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost;
+    if (totalCost === laborCost + materialCost) { alert("The cost equals"); }
+    if (true) { alert("True"); }
+    if (false) { alert("False"); }
+
+    if ("car" < "cat") { alert("car is smaller than cat"); }
+
+
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function assignment12_13task1() {
+
+};
+
+function assignment12_13task2() {
+  
+    var num1 = prompt("Enter first number:");
+    var num2 = prompt("Enter second number:");
+
+    if (num1 == num2)
+    {
+        alert("Both Number Are Equal ");
+    } 
+    else if (num1 > num2) 
+    {
+        alert(num2 + " is larger than " + num1);
+    }
+    else 
+    {
+        alert(num1 + " is less than " + num2);
+    }
+};
+
+function assignment12_13task3() {
+    var num = prompt("Enter Number for confirmation that the number is Positive , Negative or Zero ")
+    if (num > 0) {
+        alert("' " + num + " '" + " Number Is Positive")
+    }
+    else if (num < 0) {
+        alert("' " + num + " '" + " Number Is Negative")
+    }
+    else if (num == 0) {
+        alert("The Number Is Zero")
+    }
+    else {
+        alert("' " + num + " '" + " It's not Number")
+    }
+};
+
+function assignment12_13task4() {
+    var arr = ["a", "e", "i", "o", "u"]
+    var vowels = prompt("Enter Character for vowels confirmation")
+
+    if (vowels === arr[0] || arr[1] || arr[2] || arr[3] || arr[4]) {
+        alert("wovels")
+    }
+    else {
+        alert("ssssssss")
+    }
+};
+
+
+
+function assignment12_13task5() {
+    var arr = ["anas", "murtaza"];
+    var passwd = prompt(" Enter his/her Password")
+
+    if (passwd === arr[0]) {
+        var correctPasswd = prompt("Enter Your Password")
+    }
+    if (correctPasswd === arr[1]) {
+        alert("Correct! The password you entered matches the original password")
+    }
+    else {
+        alert("Incorrect pass")
+    }
+};
+
+
+function assignment12_13task6() {
+
+    var greeting;
+    greeting = prompt("Enter Hours")
+    var hour = 13;
+    if (greeting <= hour) {
+        alert("Good day");
+    }
+    else {
+        alert("Good evening");
+    }
+};
+
+function assignment12_13task7() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function assignment0task1() {
+
+};
+
+function assignment0task2() {
+
+};
+
+function assignment0task3() {
+
+
+};
+
+function assignment0task4() {
+
+};
+
+function assignment0task5() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function assignment0task1() {
+
+};
+
+function assignment0task2() {
+
+};
+
+function assignment0task3() {
+
+
+};
+
+function assignment0task4() {
+
+};
+
+function assignment0task5() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function assignment0task1() {
+
+};
+
+function assignment0task2() {
+
+};
+
+function assignment0task3() {
+
+
+};
+
+function assignment0task4() {
+
+};
+
+function assignment0task5() {
+
+};
+
+
+// ****************************************************5st ASSIGNMENT ENDS************************************************************
